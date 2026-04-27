c7be798c85e13d8c5d6aa071f86716e9097adf14
MixiStorm <mixitoni075@gmail.com>
2026-04-27 22:02:16 +0300

    Am adugat .gitignore

--- build/auto.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo Compilam intreg proiectul 
-cd ..
-cmake -G Ninja -B build 
-Ninja -C build
-cd build
--- build/raylib/raylib/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/goguj/Desktop/VM/raylib/src
-# Build directory: C:/Users/goguj/Desktop/VM/build/raylib/raylib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("external/glfw")
